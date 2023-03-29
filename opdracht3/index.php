aa29d62a7791e14c1c562c15e2afa5418fb7253b
Oliver <Oliverdegroot@outlook.com>
2023-03-29 09:31:09 +0100

    fiets

---
+++ opdracht3/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+
+// todo: check if password is correct.
+
+// if correct, inform the user!
+echo "SUCCESS " . $username . "!";
+
+
+?>
